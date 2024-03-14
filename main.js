65d2a8e29b59bcec3df8d68f7fc6cf717abc48af
Faiza61 <160414758+Faiza61@users.noreply.github.com>
2024-03-14 12:53:59 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+var myName = "FABIHA";
+console.log(myName);
+myName = "MANAL";
+console.log(myName);
+var age = 19;
+console.log(age);
---
+++ main.ts
@@ -0,0 +1,36 @@
+
+let myName ="FABIHA";
+console.log(myName);
+myName="MANAL";
+console.log(myName);
+
+ const age  = 19
+console.log(age);
+
+
+// class 4
+// ye variable full name k liye hai
+let fullName: string ="Faiza Shaukat"
+console.log(fullName);
+console.log(fullName);
+//console.log(fullName);
+//console.log(fullName);
+
+
+/*faiza
+28
+PST b-14
+M.A*/
+//B.ed
+
+//ERRORS'
+//syntax error
+lett teacherName ; = "Mnanal Rana";
+
+//Type error
+let message ="hellow World"
+console.loger(message);
+
+//assigibility error
+let message = "hello"
+message = 24
